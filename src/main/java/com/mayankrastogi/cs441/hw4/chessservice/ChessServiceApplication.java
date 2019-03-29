b11f30e1cc8dfcf538ff6f827f55f437e83db199
Mayank K Rastogi <mayankrastogi.is@gmail.com>
2019-03-29 02:22:22 -0500

    Create Spring Boot project

---
+++ src/main/java/com/mayankrastogi/cs441/hw4/chessservice/ChessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mayankrastogi.cs441.hw4.chessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mayankrastogi/cs441/hw4/chessservice/ChessServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mayankrastogi.cs441.hw4.chessservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChessServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
